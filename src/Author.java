d74799cf81930a2b10420872740a3ecd793eb41b
bornartem <born.artem@gmail.com>
2023-08-18 11:23:00 +0300

    1 commit

---
+++ src/Author.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class Author {
+    private String authorFirstName;
+    private String authorSurname;
+
+
+    public Author (String authorFirstName, String authorSurname){
+        this.authorFirstName = authorFirstName;
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorFirstName(){
+        return this.authorFirstName;
+    }
+    public String getAuthorSurname(){
+        return this.authorSurname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(authorFirstName, author.authorFirstName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authorFirstName);
+    }
+
+    @Override
+    public String toString(){
+        return "Автор книги - " + authorFirstName + " " + authorSurname;
+    }
+}
